efa6c960150da7ba069b4908f562760c5ae43eea
abhisheksinghjin <56029486+abhisheksinghjin@users.noreply.github.com>
2022-05-12 01:05:29 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='ML Solution to predict the future failure rates of unscheduled maintenance components and prevent them from failing again.',
+    author='Abhishek Singh',
+    license='MIT',
+)
