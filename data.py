05c39f512d58abd53141427236743b933140b8cb
abhisheksinghjin <56029486+abhisheksinghjin@users.noreply.github.com>
2022-05-13 00:41:16 +0530

    first commit and added raw data

--- data.py
+++ data.py
@@ -6,7 +6,7 @@ from glob import glob
 data_dirs = ["encoders"]
 
 for data_dir in data_dirs:
-    files = glob(data_dir + r"/*.csv")
+    files = glob(data_dir + r".csv")
     for filePath in files:
         # print(f"dvc add {filePath}")
         os.system(f"dvc add {filePath}")
